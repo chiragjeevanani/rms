98d7df34b388df495f7542809ec6e89404e611bb
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-06-04 16:25:39 +0530

    d

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const SUPERADMIN_DB_URL =
-  "mongodb://superadmin123%21%23:superadmin%40123@210.56.147.234:27017/RMS-Superadmin?authSource=admin";
+const SUPERADMIN_DB_URL = "mongodb://mohammadrehan00121_db_user:BFZebo6PR8Ripzzq@ac-pxrc1fm-shard-00-00.vydv7ur.mongodb.net:27017,ac-pxrc1fm-shard-00-01.vydv7ur.mongodb.net:27017,ac-pxrc1fm-shard-00-02.vydv7ur.mongodb.net:27017/RMS-Superadmin?ssl=true&replicaSet=atlas-112ag1-shard-0&authSource=admin&appName=Cluster0";
 
 const connectDB = async () => {
   try {
