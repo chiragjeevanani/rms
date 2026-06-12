8beea34715e90405ec1cdb80af844c2634f4aa72
Muhammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2026-06-12 13:04:45 +0530

    d

--- superadmin-backend/Config/db.js
+++ superadmin-backend/Config/db.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
 
-const SUPERADMIN_DB_URL = "mongodb://mohammadrehan00121_db_user:BFZebo6PR8Ripzzq@ac-pxrc1fm-shard-00-00.vydv7ur.mongodb.net:27017,ac-pxrc1fm-shard-00-01.vydv7ur.mongodb.net:27017,ac-pxrc1fm-shard-00-02.vydv7ur.mongodb.net:27017/RMS-Superadmin?ssl=true&replicaSet=atlas-112ag1-shard-0&authSource=admin&appName=Cluster0";
-
 const connectDB = async () => {
   try {
-    const isSuperAdmin = process.env.IS_SUPERADMIN === 'true';
-
-    const dbUrl = isSuperAdmin
-      ? SUPERADMIN_DB_URL
-      : process.env.MONGODB_URL;
+    const dbUrl = process.env.MONGODB_URL;
 
     if (!dbUrl) {
       throw new Error("MongoDB URL is missing in environment variables");
     }
 
-    console.log(`🔐 MODE: ${isSuperAdmin ? 'SUPERADMIN' : 'ADMIN'}`);
+    console.log(`🔐 MODE: SUPERADMIN`);
     console.log(`Connecting to database...`);
 
     const conn = await mongoose.connect(dbUrl);
@@ -28,4 +22,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-connectDB.SUPERADMIN_DB_URL = SUPERADMIN_DB_URL;
